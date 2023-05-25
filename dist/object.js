8ce938a9ca2b0b54a4bf050667e1bae6029c2659
MD.Omar Faruq <omarfaruq.bplus.6600@gmail.com>
2023-05-25 08:30:03 +0600

    first commit

---
+++ dist/object.js
@@ -0,0 +1,11 @@
+"use strict";
+const student = {
+    company: "programming hero",
+    name: "montu mia",
+    age: 25,
+    roll: 123,
+    phone: 123456789,
+    isMarried: true,
+    wife: "bibi",
+};
+console.log(student);
