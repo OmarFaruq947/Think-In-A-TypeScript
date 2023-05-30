bf78cc9eeed55a81c5e3df536f3a426a4a9cb2ae
MD.Omar Faruq <omarfaruq.bplus.6600@gmail.com>
2023-05-30 09:05:04 +0600

    access modifiers concept

---
+++ src/OOP/Access-Modifiers.ts
@@ -0,0 +1,29 @@
+// bank account
+
+class BankAccount {
+  id: number;
+  name: string;
+  private _balance: number;
+
+  constructor(id: number, name: string, balance: number) {
+    this.id = id;
+    this.name = name;
+    this._balance = balance;
+  }
+
+  getBalance() {
+    console.log(`my current balance is ${this._balance}`);
+  }
+
+  addDeposit(amount: number) {
+    this._balance = this._balance + amount;
+  }
+}
+
+class StudentAccount extends BankAccount {
+  test() {}
+}
+
+const myAccount = new BankAccount(102, "omar", 200);
+
+console.log(myAccount);
